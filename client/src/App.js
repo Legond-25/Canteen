92051451441f99b8cc6151ddf26121018f39cd09
NanditaNikam <nandita.nikam_comp2020@pccoer.in>
2022-04-05 18:00:06 +0530

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Home/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Registration from './components/Home/Registration';
 
+
 function App() {
   return (
     <Router>
